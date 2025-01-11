a6dd9dc6baaf50e80773bc5791ad2226cb2ad7ac
Danil019 <100717092+Danil019@users.noreply.github.com>
2025-01-11 21:27:17 +0300

    Add files via upload

---
+++ lima.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7663863093:AAHBqmiFkF15GWjTPKjsmgb68wyN-Iz8QVg"
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+@dp.message_handler(commands = 'start')
+async def hello_word(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью. Но это не точно')
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates = True)
